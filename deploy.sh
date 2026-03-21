6706dd9af8b2e13de179ad773fa1288fe47d4c83
wediga <98690722+wediga@users.noreply.github.com>
2026-03-21 07:09:12 +0000

    Sync from internal repository (PR #13)

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -e
 git config --global --add safe.directory /home/wediga/arcanalyse
 
 cd /home/wediga/arcanalyse
+git reset --hard origin/main
 git pull origin main
 cd frontend
 docker compose build
